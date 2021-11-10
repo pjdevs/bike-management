620a45a78a019dbc33a99bf3ed4f1734283f0e8e
pjdevs <pj33720@gmail.com>
2021-11-10 15:42:03 +0100

    Added geo points in stations and start triggers

--- db/populate_db.sql
+++ db/populate_db.sql
@@ -26,27 +26,29 @@ commit;
 
 -- STATIONS
 
-insert into STATIONS values (1, '1 Avenue du Dr Albert Shweitzer 33400 Talence', 10, 3);
-insert into STATIONS values (2, 'Place Pey Berland 33000 Bordeaux', 30, 1); 
+insert into STATIONS values (1, '1 Avenue du Docteur Albert Schweitzer, 33400 Talence', 1, 3, 44.792864, -0.623373);
+insert into STATIONS values (2, 'Place Pey Berland 33000, Bordeaux', 30, 1, 44.83772845,-0.5765286471688031); 
 
 commit;
 
 -- VELOS
 
 insert into VELOS values (1, 'Tilt 500 E', "B'Twin", date('2021-11-08'), 0, 'NEUF', 100, 1);
-insert into VELOS values (2, 'ELOPS 920', "ELOPS", date('2021-10-08'), 50, 'BON', 70, NULL); 
+insert into VELOS values (2, 'ELOPS 920', "ELOPS", date('2021-10-08'), 45, 'BON', 70, NULL); 
 
 commit;
 
 -- ADHERENTS
 
-insert into ADHERENTS values (1, 'Morel', 'PJ', '7 Rue Salvador Allende 33400 Talence', date('2021-11-08'), 1);
+insert into ADHERENTS values (1, 'MOREL', 'Pierre-Jean', '7 Rue Salvador Allende, 33400 Talence', date('2021-11-08'), 3);
+insert into ADHERENTS values (2, 'MARAIS', 'Lucas', ',7 Rue Léo Ferré 33400 Pessac', date('2021-11-08'), 2);
+insert into ADHERENTS values (3, 'MEDINA', 'Enzo', '7 Rue Georges Bonnac, 33000 Bordeaux', date('2021-11-08'), 1);
 
 commit;
 
 -- EMPRUNTS
 
-insert into EMPRUNTS values (1, date('2021-11-08'), time('23:21:00'), 10, date('2021-11-09'), time('05:48:54'), 20, 1, 1, 1, 1);
+insert into EMPRUNTS values (1, date('2021-11-08'), time('23:21:00'), 45, 1, NULL, NULL, NULL, NULL, 1, 2);
 
 commit;
 
